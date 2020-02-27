83224c6d8887d05f28c3dff74dbdd8f16dddfdaf
Nelson Mbau <mbau.ndungu@gmail.com>
2020-02-27 08:10:28 +0300

    feat: Basic hello world service for go-micro

---
+++ main.go
@@ -0,0 +1,30 @@
+package main;
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/mbau_ndungu/go-micro-hello/proto"
+	"github.com/micro/go-micro"
+)
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *pb.Request, rsp *pb.Response) error {
+	rsp.Greeting = "Hello " + req.Name
+	return nil
+}
+
+func main() {
+	service := micro.NewService(
+		micro.Name("helloworld"),
+	)
+
+	service.Init()
+
+	pb.RegisterGreeterHandler(service.Server(), new(Greeter))
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
